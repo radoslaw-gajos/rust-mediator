cf41366bc9bf8a4119f55ff367c4e68e5d384136
radoslaw-gajos <radoslawgajos@gmail.com>
2024-10-13 00:57:59 +0200

    I thought doing this would be easier

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,81 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use uuid::Uuid;
+use std::sync::{Arc, Mutex};
 
 trait Widget {
-    fn changed(widget: Box<dyn Widget>) {
-        self.director().borrow_mut().widget_changed(self);
-    }
-    fn director(widget: Box<dyn Widget>) -> Rc<RefCell<dyn Director>>;
-}
-
-trait Director {
-    fn widget_changed(&mut self, widget: Box<dyn Widget>);
 }
 
 struct RadioWidget {
-    value: bool,
-    director: Rc<RefCell<dyn Director>>,
+}
+
+impl Widget for RadioWidget {
 }
 
 impl RadioWidget {
-    fn set_value(&self, value: bool) {
-        self.value = value;
-        self.changed();
+    fn new() -> Mutex<Box<dyn Widget>> {
+        Mutex::new(Box::new(RadioWidget {
+        }))
     }
 }
 
-struct FoodDirector {
-    likes_pizza: RadioWidget,
+trait WidgetRepository {
+    fn add(&mut self, widget: Arc<Mutex<Box<dyn Widget>>>) -> Uuid;
+    fn get(&self, uuid: Uuid) -> Arc<Mutex<Box<dyn Widget>>>;
+    fn remove(&mut self, uuid: Uuid);
 }
+
+struct HMapWidgetRepository {
+}
+
+static WIDGET_REPOSITORY: Mutex<HMapWidgetRepository> = Mutex::new(HMapWidgetRepository {
+});
+
+impl WidgetRepository for HMapWidgetRepository {
+    fn add(&mut self, widget: Arc<Mutex<Box<dyn Widget>>>) -> Uuid {
+        Uuid::new_v4()
+    }
+    fn get(&self, uuid: Uuid) -> Arc<Mutex<Box<dyn Widget>>> {
+        panic!("Not implemented yet");
+    }
+    fn remove(&mut self, uuid: Uuid) {
+        panic!("Not implemented yet");
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    struct MockDirector {
+    struct MockWidget {
+        id: String,
+    }
+
+    impl Widget for MockWidget {
+    }
+
+    impl MockWidget {
+        fn new(id: String) -> Mutex<Box<dyn Widget>> {
+            Mutex::new(Box::new(MockWidget {
+                id
+            }))
+        }
     }
 
     #[test]
-    fn setting_value_should_call_director() {
+    fn should_add_and_get_widget() {
+        // given
+        let expected = Arc::new(MockWidget::new("widget_id".to_string()));
+
+        // when
+        let uuid = WIDGET_REPOSITORY.lock().unwrap().add(expected); 
+        /*
+        let actual: Box<MockWidget> = 
+            (*WIDGET_REPOSITORY.lock().unwrap().get(uuid).lock().unwrap()).as_ref().downcast_ref::<MockWidget>();
+            */
+        let actual: Box<MockWidget> = 
+            WIDGET_REPOSITORY.lock().unwrap().get(uuid).lock().unwrap().downcast_ref::<MockWidget>();
+
+        // then
+        //assert_eq!();
     }
 }
