1be3e21382002d728431fa4fbf1479753384de0f
radoslaw-gajos <radoslawgajos@gmail.com>
2024-10-12 23:43:32 +0200

    Working on references

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,27 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 trait Widget {
-    fn Changed(&self) {
+    fn changed(&self) {
+        let widget: Box<&dyn Widget> = Box::new(self);
+        self.director().borrow_mut().widget_changed(widget);
     }
     fn director(&self) -> Rc<RefCell<dyn Director>>;
 }
 
 trait Director {
-    fn WidgetChanged(&mut self, widget: &Box<dyn Widget>);
+    fn widget_changed(&mut self, widget: Box<dyn Widget>);
 }
 
 struct RadioWidget {
     value: bool,
     director: Rc<RefCell<dyn Director>>,
+}
+
+impl RadioWidget {
+    fn set_value(&self, value: bool) {
+        self.value = value;
+        self.changed();
+    }
 }
 
 struct FoodDirector {
