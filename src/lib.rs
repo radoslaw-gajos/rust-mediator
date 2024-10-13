94e0a079480a74af0d1cffb2086d14cf861e4f3d
radoslaw-gajos <radoslawgajos@gmail.com>
2024-10-13 03:28:47 +0200

    Trying to make mediator work

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,52 @@
-use uuid::Uuid;
-use std::sync::{Arc, Mutex};
-
 trait Widget {
+    fn changed(&self, director: &Box<dyn Director>);
 }
 
 struct RadioWidget {
 }
 
 impl Widget for RadioWidget {
+    fn changed(&self, director: &Box<dyn Director>) {
+    }
 }
 
 impl RadioWidget {
-    fn new() -> Mutex<Box<dyn Widget>> {
-        Mutex::new(Box::new(RadioWidget {
-        }))
+    fn new() -> Box<dyn Widget> {
+        Box::new(RadioWidget {})
     }
 }
 
-trait WidgetRepository {
-    fn add(&mut self, widget: Arc<Mutex<Box<dyn Widget>>>) -> Uuid;
-    fn get(&self, uuid: Uuid) -> Arc<Mutex<Box<dyn Widget>>>;
-    fn remove(&mut self, uuid: Uuid);
+trait Director {
+    fn widget_changed(&self, widget: &Box<dyn Widget>);
 }
 
-struct HMapWidgetRepository {
+struct FoodDirector {
+    radio: Box<dyn Widget>,
 }
 
-static WIDGET_REPOSITORY: Mutex<HMapWidgetRepository> = Mutex::new(HMapWidgetRepository {
-});
-
-impl WidgetRepository for HMapWidgetRepository {
-    fn add(&mut self, widget: Arc<Mutex<Box<dyn Widget>>>) -> Uuid {
-        Uuid::new_v4()
-    }
-    fn get(&self, uuid: Uuid) -> Arc<Mutex<Box<dyn Widget>>> {
-        panic!("Not implemented yet");
-    }
-    fn remove(&mut self, uuid: Uuid) {
-        panic!("Not implemented yet");
+impl Director for FoodDirector {
+    fn widget_changed(&self, widget: &Box<dyn Widget>) {
     }
 }
 
+impl FoodDirector {
+    fn new(radio: Box<dyn Widget>) -> Box<dyn Director> {
+        Box::new(FoodDirector {
+            radio,
+        })
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    struct MockWidget {
-        id: String,
-    }
-
-    impl Widget for MockWidget {
-    }
-
-    impl MockWidget {
-        fn new(id: String) -> Mutex<Box<dyn Widget>> {
-            Mutex::new(Box::new(MockWidget {
-                id
-            }))
-        }
-    }
-
     #[test]
-    fn should_add_and_get_widget() {
-        // given
-        let expected = Arc::new(MockWidget::new("widget_id".to_string()));
+    fn foo() {
+        let radio: Box<dyn Widget> = RadioWidget::new();
+        let director: Box<dyn Director> = FoodDirector::new(radio);
 
-        // when
-        let uuid = WIDGET_REPOSITORY.lock().unwrap().add(expected); 
-        /*
-        let actual: Box<MockWidget> = 
-            (*WIDGET_REPOSITORY.lock().unwrap().get(uuid).lock().unwrap()).as_ref().downcast_ref::<MockWidget>();
-            */
-        let actual: Box<MockWidget> = 
-            WIDGET_REPOSITORY.lock().unwrap().get(uuid).lock().unwrap().downcast_ref::<MockWidget>();
-
-        // then
-        //assert_eq!();
+        //radio.changed(&director);
+        //director.widget_changed(&radio);
     }
 }
