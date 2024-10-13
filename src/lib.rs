be03d06574db6eb5c5b4df52b97a773ddc074453
radoslaw-gajos <radoslawgajos@gmail.com>
2024-10-13 18:37:54 +0200

    Fighting with downcasting

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::cell::RefCell;
 
 trait Widget {
     fn changed(&self, director: &Box<dyn Director>);
-    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&self) -> &mut dyn Any;
 }
 
 struct RadioWidget {
@@ -16,7 +16,7 @@ struct RadioWidget {
 impl Widget for RadioWidget {
     fn changed(&self, director: &Box<dyn Director>) {
     }
-    fn as_any(&self) -> &dyn Any {
+    fn as_any_mut(&mut self) -> &mut dyn Any {
         self
     }
 }
@@ -39,14 +39,14 @@ struct FoodDirector {
     radio: Box<dyn Widget>,
 }
 
-fn radio_from_widget(widget: &Box<dyn Widget>) -> &RadioWidget{
-    widget.as_any()
-        .downcast_ref::<RadioWidget>()
+fn radio_from_widget(widget: &mut Box<dyn Widget>) -> &mut RadioWidget {
+    widget.as_any_mut()
+        .downcast_mut::<RadioWidget>()
         .unwrap()
 }
 
 impl Director for FoodDirector {
-    fn widget_changed(&self, widget: &str) {
+    fn widget_changed(&mut self, widget: &str) {
         match widget {
             "radio" => {
                 println!("Radio changed");
@@ -90,19 +90,21 @@ mod tests {
         let director: Box<dyn Director> = FoodDirector::new(radio);
 
         let food_dir = food_dir_from_widget(&director);
-        let radio = radio_from_widget(&food_dir.radio);
+        let mut radio = radio_from_widget(&food_dir.radio);
 
         //let director = Rc::new(director);
         //radio.director = Some(Rc::clone(&director));
         //let radio_director = radio.director;
-        let radio_director = &radio.director;
+        //let radio_director = &radio.director;
         /*
         if let Some(rd) = *radio_director.borrow_mut() {
         }
         */
         //let radio_director = *radio_director.borrow_mut()
         //(*radio_director.borrow_mut().unwrap()) = *director;
-        *radio_director.borrow_mut() = Some(director);
+        let rc_director = Rc::new(RefCell::new(Some(director)));
+        //*radio_director.borrow_mut() = Some(director);
+        radio.director = Rc::clone(&rc_director);
 
         director.widget_changed("radio");
     }
