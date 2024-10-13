3a2623016a79c6cfd6ec22f0ce5ad19f99381591
radoslaw-gajos <radoslawgajos@gmail.com>
2024-10-13 18:53:56 +0200

    Implementing Mediator in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,39 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::collections::HashMap;
+
 trait Director {
+    fn attach(&mut self, key: String, widget: Box<dyn Widget>);
 }
 
 trait Widget {
 }
 
 struct FoodDirector {
+    widgets: HashMap<String, Box<dyn Widget>>,
 }
 
 impl Director for FoodDirector {
+    fn attach(&mut self, key: String, widget: Box<dyn Widget>) {
+    }
+}
+
+impl FoodDirector {
+    fn new() -> Box<dyn Director> {
+        Box::new(FoodDirector {
+            widgets: HashMap::new(),
+        })
+    }
 }
 
 struct RadioWidget {
+    director: Rc<RefCell<Box<dyn Director>>>,
+}
+
+fn attach(
+    director: Rc<RefCell<Box<dyn Director>>>,
+    key: String,
+    widget: Box<dyn Widget>) {
 }
 
 impl Widget for RadioWidget {
@@ -22,5 +45,6 @@ mod tests {
 
     #[test]
     fn foo() {
+        let director = FoodDirector::new();
     }
 }
