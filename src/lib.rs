f96be0d2de2c310bbdca6c2870610ac4e5c2d8ba
radoslaw-gajos <radoslawgajos@gmail.com>
2024-10-14 22:00:37 +0200

    Adding widgets to FoodDirector

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use crate::director::Director;
 
 pub mod director {
+    use crate::widget::Widget;
+
+    use std::rc::Rc;
+
     pub trait Director {
+        fn attach(&self, key: String, widget: Rc<dyn Widget>);
+        fn get(&self, key: String) -> Rc<dyn Widget>;
     }
 }
 
@@ -18,13 +24,50 @@ pub mod food_director {
     }
 
     impl Director for FoodDirector {
+        fn attach(&self, key: String, widget: Rc<dyn Widget>) {
+            self.widgets
+                .borrow_mut()
+                .insert(key, widget);
+        }
+        fn get(&self, key: String) -> Rc<dyn Widget> {
+            match self.widgets
+                .borrow()
+                .get(&key) {
+                    Some(widget) => Rc::clone(widget),
+                    None => panic!("invalid key"),
+            }
+        }
     }
 
     impl FoodDirector {
-        pub fn new() -> Rc<dyn Director> {
+        fn new() -> Rc<dyn Director> {
             Rc::new(FoodDirector {
                 widgets: RefCell::new(HashMap::new()),
             })
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        struct MockWidget;
+
+        impl Widget for MockWidget {
+        }
+
+        #[test]
+        fn should_add_a_widget() {
+            // given
+            let director = FoodDirector::new();
+            let widget: Rc<dyn Widget> = Rc::new(MockWidget);
+
+            // when
+            director.attach("mock".to_string(), Rc::clone(&widget));
+            let result = director.get("mock".to_string());
+
+            // then
+            assert!(Rc::ptr_eq(&widget, &result));
         }
     }
 }
